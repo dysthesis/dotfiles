51bf8d366129554cc69a2de9813366e57c66c3bd
Dysthesis <antheoraviel@protonmail.com>
2024-02-06 20:24:18 +1100

    Start neovim configuration from scratch

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,4 @@
+-- Set up lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -10,4 +11,26 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup({ {import = 'plugins'} },{})
+
+require("lazy").setup({
+    -- Plugins
+    -- Import the lua/plugins directory
+    -- Git related plugins
+    'tpope/vim-fugitive',
+    'tpope/vim-rhubarb',
+
+    -- Detect tabstop and shiftwidth automatically
+    'tpope/vim-sleuth',
+    { 'folke/which-key.nvim', opts = {} },
+    { import = 'plugins' },
+  },
+  {
+    -- Options
+  })
+
+-- Modules
+require("config.keybinds")
+require("config.autocomplete")
+require("config.lsp")
+require("config.telescope")
+require("config.treesitter")
