6b234af9f2be769eed14593b79709a669645af6a
Dysthesis <antheoraviel@protonmail.com>
2024-06-24 11:49:13 +1000

    Fish bindings for !! and !$

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -113,6 +113,41 @@ function dotpush
 end
 
 
+# Functions needed for !! and !$
+function __history_previous_command
+  switch (commandline -t)
+  case "!"
+    commandline -t $history[1]; commandline -f repaint
+  case "*"
+    commandline -i !
+  end
+end
+
+function __history_previous_command_arguments
+  switch (commandline -t)
+  case "!"
+    commandline -t ""
+    commandline -f history-token-search-backward
+  case "*"
+    commandline -i '$'
+  end
+end
+
+# The bindings for !! and !$
+if [ "$fish_key_bindings" = "fish_vi_key_bindings" ];
+  bind -Minsert ! __history_previous_command
+  bind -Minsert '$' __history_previous_command_arguments
+else
+  bind ! __history_previous_command
+  bind '$' __history_previous_command_arguments
+end
+
+# Function for creating a backup file
+# ex: backup file.txt
+# result: copies file as file.txt.bak
+function backup --argument filename
+    cp $filename $filename.bak
+end
 # Variables
 export VDIRSYNCER_CONFIG=$HOME/.config/vdirsyncer/config
 export ZK_NOTEBOOK_DIR=~/Documents/Notes
