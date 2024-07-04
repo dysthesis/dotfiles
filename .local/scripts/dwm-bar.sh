6b7d7b719b8bbbaf6dc708eb363c2bdee523b2c8
Dysthesis <antheoraviel@protonmail.com>
2024-07-05 08:17:26 +1000

    scripts: add taskwarrior module to `dwm-bar.sh` and clean up

--- .local/scripts/dwm-bar.sh
+++ .local/scripts/dwm-bar.sh
@@ -10,53 +10,36 @@ cpu() {
   cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
 
   printf "^c#000000^ ^b#f9e2af^  "
-  printf "^c#ffffff^ ^b#000000^ $cpu_val"
-}
-
-pkg_updates() {
-  updates=$({ timeout 20 doas xbps-install -un 2>/dev/null || true; } | wc -l) # void
-  #updates=$({ timeout 20 checkupdates 2>/dev/null || true; } | wc -l) # arch
-  # updates=$({ timeout 20 aptitude search '~U' 2>/dev/null || true; } | wc -l)  # apt (ubuntu, debian etc)
-  
-  printf "  ^c#000000^ ^b#ABE9B3^  "
-  if [ -z "$updates" ]; then
-    printf "^c#ffffff^ ^b#000000^ Fully Updated"
-  else
-    printf "^c#ffffff^ ^b#000000^ $updates"" updates"
-  fi
-}
-
-battery() {
-  get_capacity="$(cat /sys/class/power_supply/BAT1/capacity)"
-  printf "^c#96CDFB^   $get_capacity"
-}
-
-brightness() {
-  printf "^c#F28FAD^   "
-  printf "^c#F28FAD^%.0f\n" $(cat /sys/class/backlight/*/brightness)
+  printf "^c#ffffff^ ^b#000000^ $cpu_val "
 }
 
 mem() {
   printf "^c#000000^^b#cba6f7^   "
-  printf "^b#000000^ ^c#ffffff^ $(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
-}
-
-wlan() {
-	case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
-	up) printf "^c#000000^ ^b#96CDFB^ 󰤨 ^d^%s" " ^c#96CDFB^Connected" ;;
-	down) printf "^c#000000^ ^b#96CDFB^ 󰤭 ^d^%s" " ^c#96CDFB^Disconnected" ;;
-	esac
+  printf "^b#000000^ ^c#ffffff^$(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
 }
 
 clock() {
-	printf "^c#000000^ ^b#83bae8^ 󱑆  "
-	printf "^c#ffffff^^b#000000^ $(date '+%H:%M')  "
+  printf "^c#000000^ ^b#83bae8^ 󱑆  "
+  printf "^c#ffffff^^b#000000^ $(date "+%Y-%m-%d %H:%M")  "
+}
+
+taskwarrior() {
+  printf "^b#a6e3a1^^c#000000^   "
+  is_ready=$(timeout 10s task ready)
+  if [ -z is_ready ]
+  then
+    printf "^c#ffffff^^b#000000^ No tasks "
+  else
+    next_id=$(timeout 10s task next limit:1 | tail -n +4 | head -n 1 | sed 's/^ //' | cut -d ' ' -f1)
+    next_desc=$(timeout 10s task _get ${next_id}.description)
+    next_due=$(timeout 10s task _get ${next_id}.due | cut -dT -f1)
+    printf "^c#ffffff^^b#000000^  $next_desc due $next_due"
+  fi
 }
 
 while true; do
-
-  [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
+  [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] 
   interval=$((interval + 1))
 
-  sleep 1 && xsetroot -name "$updates $(cpu) $(mem) $(clock)"
+  sleep 1 && xsetroot -name "$(taskwarrior) $(cpu) $(mem) $(clock)"
 done
