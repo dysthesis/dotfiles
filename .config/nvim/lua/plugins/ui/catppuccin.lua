d98841e68fb44e94d47dc2091c7a383b06c3abc2
Dysthesis <antheoraviel@protonmail.com>
2024-07-05 11:28:01 +1000

    neovim: colourscheme tweaks

--- .config/nvim/lua/plugins/ui/catppuccin.lua
+++ .config/nvim/lua/plugins/ui/catppuccin.lua
@@ -1,15 +1,15 @@
 return { -- Colourscheme
   'catppuccin/nvim',
   name = 'catppuccin',
+  enabled = true,
   priority = 1000,
   config = function()
     require('catppuccin').setup {
       transparent_background = true,
-      color_overrides = {
-        all = {
-          text = '#ffffff',
-        },
-      },
+      color_overrides = { all = {
+        text = '#ffffff',
+        surface0 = '#181825',
+      } },
     }
     vim.cmd.colorscheme 'catppuccin'
   end,
--- .config/nvim/lua/plugins/ui/tokyo-night.lua
+++ .config/nvim/lua/plugins/ui/tokyo-night.lua
@@ -6,7 +6,6 @@ return {
       'nvim-lualine/lualine.nvim',
       optional = true,
       config = function()
-        -- Lua
         require('lualine').setup {
           options = {
             -- ... your lualine config
@@ -17,45 +16,13 @@ return {
       end,
     },
   },
-  init = function ()
-    vim.cmd[[colorscheme tokyonight]]
-  end,
-  config = function ()
-    require("tokyonight").setup({
-      transparent = true,
-        on_highlights = function(hl, c)
-    local prompt = "#2d3149"
-    hl.TelescopeNormal = {
-      bg = c.bg_dark,
-      fg = c.fg_dark,
-    }
-    hl.TelescopeBorder = {
-      bg = c.bg_dark,
-      fg = c.bg_dark,
-    }
-    hl.TelescopePromptNormal = {
-      bg = prompt,
-    }
-    hl.TelescopePromptBorder = {
-      bg = prompt,
-      fg = prompt,
-    }
-    hl.TelescopePromptTitle = {
-      bg = prompt,
-      fg = prompt,
-    }
-    hl.TelescopePreviewTitle = {
-      bg = c.bg_dark,
-      fg = c.bg_dark,
-    }
-    hl.TelescopeResultsTitle = {
-      bg = c.bg_dark,
-      fg = c.bg_dark,
-    }
-  end,
-    })
+  init = function()
+    vim.cmd [[colorscheme tokyonight]]
   end,
   lazy = false,
   priority = 1000,
-  opts = {},
+  opts = {
+    transparent = true,
+    style = 'night',
+  },
 }
