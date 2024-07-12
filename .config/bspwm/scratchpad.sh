7f509835f74b0d48dae63482a1fcd2e421d39853
Dysthesis <antheoraviel@protonmail.com>
2024-07-12 14:27:38 +1000

    a whole lotta changes

--- .config/bspwm/scratchpad.sh
+++ .config/bspwm/scratchpad.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-case "$1" in
-
-    term)
-        entry="st -g 180x30 -c term"
-        ;;
-    notes)
-        entry="st -g 180x30 -c notes -e sh -c tmux attach-session -t notes || tmux new-session -s notes -c ~/Documents/Notes/"
-        ;;
-    task)
-        entry="st -g 180x30 -c task -e taskwarrior-tui"
-        ;;
-    files)
-        entry="st -g 180x30 -c files -e yazi"
-        ;;
-esac
+# case "$1" in
+#
+#     term)
+#         entry="st -g 180x30 -c term"
+#         ;;
+#     notes)
+#         entry="st -g 180x30 -c notes -e sh -c tmux attach-session -t notes || tmux new-session -s notes -c ~/Documents/Notes/"
+#         ;;
+#     task)
+#         entry="st -g 180x30 -c task -e taskwarrior-tui"
+#         ;;
+#     files)
+#         entry="st -g 180x30 -c files -e yazi"
+#         ;;
+# esac
 
 
 
@@ -23,6 +23,18 @@ if xdotool search --onlyvisible --class "$1" >/dev/null 2>&1; then
 elif xdotool search --class "$1" >/dev/null 2>&1; then
     xdotool search --class "$1" windowmap
 else
-
-    $entry &
+    case "$1" in
+        notes)
+            st -g 180x30 -c notes -e sh -c tmux attach-session -t notes || tmux new-session -s notes -c ~/Documents/Notes/
+            ;;
+        term)
+            st -g 180x30 -c term
+            ;;
+        task)
+            st -g 180x30 -c task -e taskwarrior-tui
+            ;;
+        files)
+            st -g 180x30 -c files -e yazi
+            ;;
+    esac
 fi
