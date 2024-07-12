a42a742ce47ba1cc657f1ea5c31fb352c22b28b6
Dysthesis <antheoraviel@protonmail.com>
2024-07-12 14:31:55 +1000

    a whole lotta changes

--- .config/bspwm/scratchpad.sh
+++ .config/bspwm/scratchpad.sh
@@ -1,23 +1,5 @@
 #!/bin/sh
 
-# case "$1" in
-#
-#     term)
-#         entry="st -g 180x30 -c term"
-#         ;;
-#     notes)
-#         entry="st -g 180x30 -c notes -e sh -c tmux attach-session -t notes || tmux new-session -s notes -c ~/Documents/Notes/"
-#         ;;
-#     task)
-#         entry="st -g 180x30 -c task -e taskwarrior-tui"
-#         ;;
-#     files)
-#         entry="st -g 180x30 -c files -e yazi"
-#         ;;
-# esac
-
-
-
 if xdotool search --onlyvisible --class "$1" >/dev/null 2>&1; then
     xdotool search --class "$1" windowunmap
 elif xdotool search --class "$1" >/dev/null 2>&1; then
@@ -35,6 +17,12 @@ else
             ;;
         files)
             st -g 180x30 -c files -e yazi
+            ;;
+        calendar)
+            st -g 180x30 -c calendar -e ikhal
+            ;;
+        Signal)
+            signal-desktop
             ;;
     esac
 fi
