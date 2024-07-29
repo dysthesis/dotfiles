908d734e01413ffb35af483a3f74cb272aac908c
Dysthesis <antheoraviel@protonmail.com>
2024-07-29 10:38:28 +1000

    changes

--- .local/scripts/notes-backup.sh
+++ .local/scripts/notes-backup.sh
@@ -11,7 +11,7 @@ echo "Initialising backup of notes at directory $(pwd) at $TIME" >> $LOGS_DIR
 BRANCH=$(git symbolic-ref --short HEAD)
 REMOTES=$(git remote)
 
-echo "On branch $BRANCH with remotes:\n" >> $LOGS_DIR
+echo "\nOn branch $BRANCH with remotes:\n" >> $LOGS_DIR
 for remote in $REMOTES
 do
 	echo "- $remote\n" >> $LOGS_DIR
@@ -23,17 +23,17 @@ do
 	git pull $remote $BRANCH 2>> $LOGS_DIR >> $LOGS_DIR
 done
 
-echo "Adding files in the current working directory." >> $LOGS_DIR
+echo "\nAdding files in the current working directory." >> $LOGS_DIR
 git add . 2>> $LOGS_DIR >> $LOGS_DIR
 
 FROM="$(whoami)@$(uname -n)"
 COMMIT_MESSAGE="Automatic backup at $TIME from $FROM"
-echo "Committing the current changes with the message '$COMMIT_MESSAGE'" >> $LOGS_DIR
+echo "\nCommitting the current changes with the message '$COMMIT_MESSAGE'" >> $LOGS_DIR
 git commit -am "$COMMIT_MESSAGE" 2>> $LOGS_DIR >> $LOGS_DIR
 
 for remote in $REMOTES
 do
-	 echo "Pushing to $remote/$BRANCH\n" >> $LOGS_DIR
+	 echo "\nPushing to $remote/$BRANCH" >> $LOGS_DIR
 	 git push $remote $BRANCH 2>> $LOGS_DIR >> $LOGS_DIR
 done
 
