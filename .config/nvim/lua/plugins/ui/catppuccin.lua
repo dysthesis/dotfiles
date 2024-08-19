88e16105ff9fd8f0cd81c02b84b7db3d58994eef
Dysthesis <antheoraviel@protonmail.com>
2024-08-19 20:40:38 +1000

    nvim: catppuccin treesitter highlighting tweaks

--- .config/nvim/lua/plugins/ui/catppuccin.lua
+++ .config/nvim/lua/plugins/ui/catppuccin.lua
@@ -10,6 +10,12 @@ return { -- Colourscheme
         text = '#ffffff',
         surface0 = '#181825',
       } },
+      custom_highlights = function(colors)
+        return {
+          ['@markup.math'] = { fg = colors.mauve },
+          ['@function.latex'] = { fg = colors.mauve },
+        }
+      end,
     }
     vim.cmd.colorscheme 'catppuccin'
   end,
