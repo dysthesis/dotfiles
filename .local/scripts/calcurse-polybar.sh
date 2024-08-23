8b9565cedd2986d522f4790bcdb5ea7972c9c247
Dysthesis <antheoraviel@protonmail.com>
2024-08-23 21:22:55 +1000

    changes

--- .local/scripts/calcurse-polybar.sh
+++ .local/scripts/calcurse-polybar.sh
@@ -1,34 +1,66 @@
 #!/bin/sh
 
-# Get the current and next day in the desired format
-TODAY=$(date +"%a, %b %d")
-TOMORROW=$(date -d "tomorrow" +"%a, %b %d")
+CALCURSE_OUTPUT=$(calcurse -n)
 
-# Retrieve the description of the next event
-DESCRIPTION=$(calcurse -d 7 --format-apt='%m' | sed -n 2p)
-
-# Retrieve the time of the next event and replace with "Today" or "Tomorrow"
-TIME=$(calcurse -n --format-apt='%(start:%a, %b %d) %(start:%H:%M) - %(end:%H:%M)\n' | sed -n 2p | sed -e "s/$TODAY/Today,/" -e "s/$TOMORROW/Tomorrow,/")
-
-# Function to print usage
-print_usage() {
-	echo "Usage: $0 {description|time|both}"
-	exit 1
+parse_calcurse_output() {
+	local output
+	output="$1"
+	local time
+	# Check if output is empty
+	if [ -z "$output" ]; then
+		HOUR=""
+		MINUTE=""
+		DESC=""
+		return
+	fi
+	time=$(printf '%s' "$output" | grep -o '\[[0-9]\{2\}:[0-9]\{2\}\]')
+	HOUR=$(printf '%s' "$time" | cut -d':' -f1 | tr -d '[]')
+	MINUTE=$(printf '%s' "$time" | cut -d':' -f2 | tr -d '[]')
+	DESC=$(printf '%s' "$output" | sed 's/.*\[[0-9]\{2\}:[0-9]\{2\}\] //' | sed -n 2p)
 }
 
-# Check the argument passed and print accordingly
-case "$1" in
-description)
-	echo "$DESCRIPTION"
-	;;
-time)
-	echo "$TIME"
-	;;
-both)
-	echo "$DESCRIPTION"
-	echo "$TIME"
-	;;
-*)
-	print_usage
-	;;
-esac
+print_time_until_event() {
+	local hours_left minutes_left
+	hours_left="$1"
+	minutes_left="$2"
+	# Handle empty hour and minute cases
+	if [ -z "$hours_left" ] || [ -z "$minutes_left" ]; then
+		return
+	fi
+
+	if [ "$hours_left" -ne 0 ]; then
+		printf 'in %d hour' "$hours_left"
+		[ "$hours_left" -gt 1 ] && printf 's'
+		[ "$minutes_left" -ne 0 ] && printf ' and '
+	fi
+
+	if [ "$minutes_left" -ne 0 ]; then
+		printf 'in %d minute' "$minutes_left"
+		[ "$minutes_left" -gt 1 ] && printf 's'
+	fi
+	printf '\n'
+}
+
+main() {
+	if [ -z "$1" ]; then
+		printf 'Usage: %s time|desc\n' "$0" >&2
+		return 1
+	fi
+
+	parse_calcurse_output "$CALCURSE_OUTPUT"
+
+	case "$1" in
+	time)
+		print_time_until_event "$HOUR" "$MINUTE"
+		;;
+	desc)
+		printf '%s\n' "$DESC"
+		;;
+	*)
+		printf 'Invalid argument. Use "time" or "desc".\n' >&2
+		return 1
+		;;
+	esac
+}
+
+main "$@"
