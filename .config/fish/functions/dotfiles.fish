e6733f368cc39ed110db79638b524272f960447e
Dysthesis <antheoraviel@protonmail.com>
2024-11-04 10:01:16 +1100

    Changes

--- .config/fish/functions/dotfiles.fish
+++ .config/fish/functions/dotfiles.fish
@@ -1,4 +1,4 @@
-function dotfiles --wraps='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"' --description 'alias dotfiles=/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
+function dots --wraps='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"' --description 'Manage dotfiles'
   /usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME" $argv
         
 end
