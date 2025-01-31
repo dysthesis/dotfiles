31b6e874b128ac4aba7e56e1ed02a9aa4d3507f0
Dysthesis <antheoraviel@protonmail.com>
2025-01-31 20:20:36 +0700

    Changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,7 +3,7 @@ if status is-interactive
     starship init fish | source
     enable_transience
     zoxide init fish --cmd cd | source
-    source "$GHOSTTY_RESOURCES_DIR"/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish
+    #source "$GHOSTTY_RESOURCES_DIR"/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish
 
     # set -x GPG_TTY (tty)
     # set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
@@ -171,3 +171,37 @@ export VDIRSYNCER_CONFIG=$HOME/.config/vdirsyncer/config
 export ZK_NOTEBOOK_DIR=~/Documents/Notes
 # export PATH="$HOME/.cargo/bin:$HOME/.cabal/bin:$HOME/.ghcup/bin:$HOME/.local/share/flatpak/exports/bin:$HOME/.local/bin:$PATH"
 export EDITOR=nvim
+
+# ____   _________________________   ____________________________________
+# 7  7   7  _  77     77  7  77  7   7  7  77     77      77     77  _  7
+# |  |   |  _  ||  ___!|   __!|  |   |  |  ||  ___!!__  __!|  ___!|    _|
+# |  !___|  7  ||  7___|     ||  !___|  |  |!__   7  7  7  |  __|_|  _ \
+# |     7|  |  ||     7|  7  ||     7|  !  |7     |  |  |  |     7|  7  |
+# !_____!!__!__!!_____!!__!__!!_____!!_____!!_____!  !__!  !_____!!__!__!
+#
+#  Name: lackluster.nvim
+#  License: MIT
+#  Maintainer: Duncan Marsh (slugbyte@slugbyte.com)
+#  Repository: https://github.com/slugbyte/lackluster.nvim
+
+# copy this code in to your fish config
+set -gx fish_color_end 7a7a7a
+set -gx fish_color_error ffaa88
+set -gx fish_color_quote 708090
+set -gx fish_color_param aaaaaa
+set -gx fish_color_option aaaaaa
+set -gx fish_color_normal CCCCCC
+set -gx fish_color_escape 789978
+set -gx fish_color_comment 555555
+set -gx fish_color_command CCCCCC
+set -gx fish_color_keyword 7a7a7a
+set -gx fish_color_operator 7788aa
+set -gx fish_color_redirection ffaa88
+set -gx fish_color_autosuggestion 2a2a2a
+set -gx fish_color_selection --background=555555
+set -gx fish_color_search_match --background=555555
+set -gx fish_pager_color_prefix 999999
+set -gx fish_pager_color_progress 555555
+set -gx fish_pager_color_completion cccccc
+set -gx fish_pager_color_description 7a7a7a
+set -gx fish_pager_color_selected_background --background=555555
